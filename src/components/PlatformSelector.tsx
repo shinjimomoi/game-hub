1c3c7751fe8cfae8c1afc9eb02d2a7efadb6cf49
Ricardo Momoi <shinjimomoi@gmail.com>
2023-10-31 00:36:40 +0900

    added platform filtering

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -6,19 +6,24 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
-const PlatformSelector = () => {
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({onSelectPlatform, selectedPlatform} :Props) => {
   const { data, error } = usePlatforms();
   if(error) return null;
   return (
     <Menu >
       <MenuButton mb={4} as={Button} rightIcon={<BsChevronDown />}>
-        Actions
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>
         ))}
       </MenuList>
     </Menu>
